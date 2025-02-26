107208f9dd5778272a81986c00a7627f4ba1b9f1
pranay reddy <pranayreddie001@gmail.com>
2025-02-25 23:33:17 -0500

    code submit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+class Solution {
+    public String defangIPaddr(String address) {
+        return address.replace(".", "[.]");
     }
 }
